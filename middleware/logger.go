67fe86dd9fb82d1b2fbfd859c6c1c06407b6aff2
Muhammad Suryono <msuryono0@gmail.com>
2025-03-29 15:13:18 +0700

    feat: print error log

--- middleware/logger.go
+++ middleware/logger.go
@@ -19,6 +19,22 @@ func LoggerMiddleware() gin.HandlerFunc {
 			appName = "yon-framework"
 		}
 
+		errors := ctx.Errors
+		if len(errors) > 0 {
+			for _, err := range errors {
+				logger.Log.Error().
+					Str("appName", appName).
+					Str("method", ctx.Request.Method).
+					Str("path", ctx.Request.URL.Path).
+					Int("status", ctx.Writer.Status()).
+					Int("size", ctx.Writer.Size()).
+					Dur("latency", time.Since(start)).
+					Str("error", err.Error()).
+					Msg("Request Failed")
+			}
+			return
+		}
+
 		logger.Log.Info().
 			Str("appName", appName).
 			Str("method", ctx.Request.Method).
